c9a0f1a3569fc2b84004be27366192c23824a2ff
Yigit Sucuoglu <yigitsucuoglu@gmail.com>
2026-07-04 02:07:21 +0300

    v0.0.8 - Add piece selection system

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,23 +1,40 @@
 import "./Board.css";
 import gameManager from "../../engine/GameManager";
 import Piece from "../Piece/Piece";
+import { useState } from "react";
 
-const game = gameManager.getGame();
+
 
 function Board() {
+  const game = gameManager.getGame();
+
+  const [, setRefresh] = useState(0);
+
   const squares = [];
 
   for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
       const piece = game.board.squares[row][col];
+      const isSelected =
+      game.selectedSquare?.row === row &&
+      game.selectedSquare?.col === col;
 
       const isLight = (row + col) % 2 === 0;
 
       squares.push(
         <div
-          key={`${row}-${col}`}
-          className={`square ${isLight ? "light" : "dark"}`}
-        >
+            key={`${row}-${col}`}
+            className={`square ${isLight ? "light" : "dark"} ${
+              isSelected ? "selected" : ""
+            }`}
+            onClick={() => {
+              if (!piece) return;
+
+              game.selectedSquare = { row, col };
+
+              setRefresh((v) => v + 1);
+            }}
+          >
           {piece && <Piece piece={piece} />}
         </div>
       );
--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,9 +1,13 @@
 import ChessBoard from "./ChessBoard";
+import type { Position } from "../types/Chess";
 
 export default class Game {
   public board: ChessBoard;
 
+  public selectedSquare: Position | null;
+
   constructor() {
     this.board = new ChessBoard();
+    this.selectedSquare = null;
   }
 }
